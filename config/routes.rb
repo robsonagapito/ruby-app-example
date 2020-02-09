970d508705ead682c042f0b326b27ab3d6ec0a68
Robson Agapito <robsonagapito@gmail.com>
2020-02-09 20:04:48 -0300

    Update frontend framework.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+
   resources :processos
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
