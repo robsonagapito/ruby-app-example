ec349c86ffc6a0a284f62d6a91e78d8b63f1bc6d
Robson Agapito <robsonagapito@gmail.com>
2020-02-09 18:49:31 -0300

    Update validations on Process attributes.

--- app/models/processo.rb
+++ app/models/processo.rb
@@ -1,2 +1,15 @@
 class Processo < ApplicationRecord
+
+	validates_presence_of :vara, message: 'não pode ser deixado em branco'
+	validates_presence_of :numero_processo, message: 'não pode ser deixado em branco'
+	validates_presence_of :natureza, message: 'não pode ser deixado em branco'
+	validates_presence_of :partes, message: 'não pode ser deixado em branco'
+	validates_presence_of :urgente, message: 'não pode ser deixado em branco'
+	validates_presence_of :arbitramento, message: 'não pode ser deixado em branco'
+	validates_presence_of :data_entrada, message: 'não pode ser deixado em branco'
+	validates_presence_of :status, message: 'não pode ser deixado em branco'
+
+	validates_format_of :urgente, :with => /\A[SN]+\z/, :message => "Somente permitido com as opções [S]im ou [N]ão."
+	validates_format_of :arbitramento, :with => /\A[SN]+\z/, :message => "Somente permitido com as opções [S]im ou [N]ão."
+
 end
