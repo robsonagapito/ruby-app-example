86b8911f9fea648d694d7ad52c0b02612b92946e
Robson Agapito <rnai@gft.com>
2023-11-14 08:02:32 -0300

    Adicionando o host/dns do qa-brazil-server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = true
+
+  config.hosts << "qa-brazil-server.gft.com"
 end
